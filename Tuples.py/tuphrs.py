f8ac27e86513165a296b4a7a39b2498e80c8663b
Okode100 <mosesokode76@gmail.com>
2024-01-25 00:06:46 +0300

    am so stressed it works

--- Tuples.py/tuphrs.py
+++ Tuples.py/tuphrs.py
@@ -1,7 +1,7 @@
 name = input("Enter file:")
 if len(name)< 1:
     name = "mbox-short.txt"
-handle = open(name)
+handle = open(name, )
 #tp_read = handle.read()#print(tp_read)
 
 count = {}
@@ -9,10 +9,10 @@ for line in handle:
     if line.startswith("From"):
         line = line.split()
         if len(line)> 5:
-            time = line[5].split(".")
+            time = line[5].split(":")
             hour = time[0]
             count[hour] = count.get(hour,0) + 1
-            count +=1
+            #count +=1
 
 for k,v in sorted(count.items()):
     print(k,v)
